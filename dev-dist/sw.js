379b63030ccbfa4caf5cba581bc235c782ac2664
berruanggawll <paus.nvl@gmail.com>
2026-07-21 14:49:19 +0700

    Save local progress before pull

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.eeevbn0nnrk"
+    "revision": "0.fnau7biv6o4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
